4ec73a273686aa4a44f222edc7e51ff4556b101c
Murad <agariomadman@gmail.com>
2024-05-30 17:23:42 -0500

    Fixed getmode.py

--- waybar/scripts/darklightmode/getmode.py
+++ waybar/scripts/darklightmode/getmode.py
@@ -3,7 +3,7 @@ import sys
 
 command = "hyprctl hyprpaper listactive"
 current_wallpaper = subprocess.run([command, '-l'], shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
-if "DP-2 = ~/.config/hypr/darkbg.jpg" in current_wallpaper:
+if "darkbg.jpg" in current_wallpaper.split("\n")[0]:
     sys.stdout.write("")
 else:
     sys.stdout.write("")
