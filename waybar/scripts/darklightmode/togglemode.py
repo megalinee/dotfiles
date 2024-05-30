5dad38f78c4cd42b7414e14d605e7111d32daeaa
Murad <agariomadman@gmail.com>
2024-05-30 17:24:00 -0500

    Fixed togglemode.py

--- waybar/scripts/darklightmode/togglemode.py
+++ waybar/scripts/darklightmode/togglemode.py
@@ -4,9 +4,9 @@ import sys
 command = "hyprctl hyprpaper listactive"
 current_wallpaper = subprocess.run([command, '-l'], shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 print(current_wallpaper)
-if "DP-2 = /home/murad/.config/hypr/darkbg.jpg" in current_wallpaper:
-    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-1,/home/murad/.config/hypr/lightbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
-    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-2,/home/murad/.config/hypr/lightbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
+if "darkbg.jpg" in current_wallpaper.split("\n")[0]:
+    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-1,~/.config/hypr/lightbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
+    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-2,~/.config/hypr/lightbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
 else:
-    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-1,/home/murad/.config/hypr/darkbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
-    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-2,/home/murad/.config/hypr/darkbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
+    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-1,~/.config/hypr/darkbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
+    subprocess.run(["hyprctl hyprpaper wallpaper \"DP-2,~/.config/hypr/darkbg.jpg\"", '-l'], shell=True, stdout=subprocess.PIPE)
