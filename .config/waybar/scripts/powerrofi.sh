fc79e4cc8cdeaf1b6a239238e74e80b5a7035c3b
megalinee <mjouhari@megaline.dev>
2024-06-14 22:44:19 -0500

    Fixed minor spacing and made some improvments

--- .config/waybar/scripts/powerrofi.sh
+++ .config/waybar/scripts/powerrofi.sh
@@ -1,32 +1,35 @@
+exit_message="󰌑 Exit Menu"
+logout_message="󰗽 Logout"
+reboot_message=" Reboot"
+shutdown_message="⏻ Shutdown"
+
+# if [ x"$@" = x"exit_message" ]
+# then
+#     exit 0
+# fi
 
 
-if [ x"$@" = x"󰌑 Exit Menu" ]
-then
-    exit 0
-fi
-
-
-if [ x"$@" = x"󰗽 Logout" ]
+if [ x"$@" = x"$logout_message" ]
 then
     hyprlock || exit 0
     
 fi
 
 
-if [ x"$@" = x" Reboot" ]
+if [ x"$@" = x"$reboot_message" ]
 then
     reboot
     exit 0 
 fi
 
 
-if [ x"$@" = x"⏻ Shutdown" ]
+if [ x"$@" = x"$shutdown_message" ]
 then
     shutdown now
     exit 0 
 fi
 
-echo "󰌑 Exit Menu"
-echo "󰗽 Logout"
-echo " Reboot"
-echo "⏻ Shutdown"
+# echo "󰌑 Exit Menu"
+echo "$logout_message"
+echo "$reboot_message"
+echo "$shutdown_message"
